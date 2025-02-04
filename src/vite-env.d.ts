92286bc35774a1946f24ffcabbc79b563192cb0e
tzachi81 <tzahih@gmail.com>
2025-02-05 01:41:49 +0200

    vercel-prep

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
 
 /// <reference types="vite-plugin-svgr/client" />
+
+VERCEL_APP_NAME="xstate-machines-pvuk.vercel.app"
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? process.env.VERCEL_APP_NAME : '/',
   plugins: [react(), svgr()],
   server: {
     proxy: {
