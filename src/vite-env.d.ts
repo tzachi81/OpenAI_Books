19c5e48cc7a7224238908808a8756dbd2c649a7a
tzachi81 <tzahih@gmail.com>
2025-02-05 02:10:22 +0200

    vercel-prep

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,4 +2,12 @@
 
 /// <reference types="vite-plugin-svgr/client" />
 
+interface ImportMetaEnv {
+  VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 VITE_API_URL="https://freetestapi.com/api/v1/books"
