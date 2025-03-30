62e9c88212af90a8846bf789ac268d7d39715a07
tzachi81 <tzahih@gmail.com>
2025-03-30 20:10:18 +0300

    Remove mock data

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,6 @@
 
 /// <reference types="vite-plugin-svgr/client" />
 
-interface ImportMetaEnv {
-  VITE_API_URL: string;
-}
-
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
-VITE_API_URL="https://freetestapi.com/api/v1/books"
