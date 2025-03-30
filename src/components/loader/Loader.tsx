a3c16f9616f28f04799779bf1574be9a6c63098a
tzachi81 <tzahih@gmail.com>
2025-03-30 20:04:08 +0300

    Style loader text

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,5 +1,10 @@
-import classes from './Loader.module.scss';
+import classes from "./Loader.module.scss";
 
 export const Loader: React.FC = () => {
-  return <div className={classes.bars}></div>
-}
+  return (
+    <div className={classes.loader}>
+      <div className={classes.bars}></div>
+      <p>Getting books using openAI, This might take a while...</p>
+    </div>
+  );
+};
